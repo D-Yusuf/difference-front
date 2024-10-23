2e3c001bf391406b1d9f8b2f33ca7fed38b143b5
hassan-mb <hassan.m.behbahani@gmail.com>
2024-10-23 19:03:00 +0300

    fixes

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "./storage";
 
-const BASE_URL = "http://192.168.8.131:8000/";
+const BASE_URL = "http://192.168.2.151:8000/api";
 
 const instance = axios.create({
   baseURL: BASE_URL + "api",
--- src/navigations/AppNav/MainNavigation.js
+++ src/navigations/AppNav/MainNavigation.js
@@ -2,12 +2,14 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Invention from "../../screens/Invention";
 import Home from "../../screens/Home";
+import Profile from "../../screens/Profile";
 const tab = createBottomTabNavigator();
 const MainNavigation = () => {
   return (
     <tab.Navigator>
       <tab.Screen name="Home" component={Home} />
       <tab.Screen name="Invention" component={Invention} />
+      <tab.Screen name="Profile" component={Profile} />
     </tab.Navigator>
   );
 };
