e4ed7f586dbd648f5c095dadb516887dfc4aa870
hassan-mb <hassan.m.behbahani@gmail.com>
2024-10-24 11:41:48 +0300

    added get user inventions feature in the profile page

--- src/components/InventionList.js
+++ src/components/InventionList.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { StyleSheet, FlatList, View } from "react-native";
+import { StyleSheet, FlatList, View, Text } from "react-native";
 import InventionCard from "./InventionCard";
 import { BASE_URL } from "../api/index";
 
-const InventionList = ({ inventions }) => {
+const InventionList = ({ profile }) => {
+  console.log("InventionList props:", { profile });
+
+  if (!profile) {
+    return <Text>Loading profile...</Text>;
+  }
+
+  const userInventions = profile.inventions || [];
+
+  console.log("User inventions:", userInventions);
+
+  if (userInventions.length === 0) {
+    return <Text>No inventions found for this user.</Text>;
+  }
+
   const renderItem = ({ item }) => <InventionCard invention={item} />;
 
   return (
     <FlatList
-      data={inventions}
+      data={userInventions}
       renderItem={renderItem}
       keyExtractor={(item) => item._id.toString()}
       contentContainerStyle={styles.container}
